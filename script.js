5c2d2317aea9eeba70b0785d1270328ec87ea082
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-03-01 22:05:45 +0100

    Finished...

--- script.js
+++ script.js
@@ -38,8 +38,9 @@ class TodoManager {
     this.taskData = [];
   }
 
-  addTask(task) {
+  addNewTask(task) {
     this.taskData.push(task);
+    renderTasks();
   }
 
   setTasks(tasks) {
@@ -53,6 +54,7 @@ class TodoManager {
   removeTask(id) {
     const updatedTasks = this.getTasks().filter((task) => task.getId() !== id);
     this.setTasks(updatedTasks);
+    renderTasks();
   }
 
   toggleTaskState(taskId, taskElement) {
@@ -65,63 +67,52 @@ class TodoManager {
   }
 }
 
-class TodoApp {
-  constructor() {
-    this.manager = new TodoManager();
-  }
+const manager = new TodoManager();
 
-  createTaskElement(id, text) {
-    const li = document.createElement("li");
-    li.id = id;
-    li.classList.add("li");
-    li.innerHTML = `${text} <div class="checkDiv">
-      <button class="checkBtn">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="check">
-          <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
-        </svg>
-      </button>
-      <button class="removeBtn">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="trash">
-          <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z" clip-rule="evenodd" />
-        </svg>
-      </button>
-    </div>`;
-    return li;
-  }
-
-  bindTaskEvents(taskElement) {
-    const removeBtn = taskElement.querySelector(".removeBtn");
-    const checkBtn = taskElement.querySelector(".checkBtn");
-
-    removeBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      taskElement.remove();
-      const taskId = taskElement.id;
-      this.manager.removeTask(taskId);
-    });
-
-    checkBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      this.manager.toggleTaskState(taskElement.id, taskElement);
-    });
-  }
-
-  renderTasks() {
-    taskList.innerHTML = "";
-    this.manager.getTasks().forEach((task) => {
-      const taskElement = this.createTaskElement(task.getId(), task.getText());
-      taskList.appendChild(taskElement);
-      this.bindTaskEvents(taskElement);
-    });
-  }
-
-  addTask(task) {
-    this.manager.addTask(task);
-    this.renderTasks();
-  }
+function createTaskElement(id, text) {
+  const li = document.createElement("li");
+  li.id = id;
+  li.classList.add("li");
+  li.innerHTML = `${text} <div class="checkDiv">
+    <button class="checkBtn">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="check">
+        <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
+      </svg>
+    </button>
+    <button class="removeBtn">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="trash">
+        <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z" clip-rule="evenodd" />
+      </svg>
+    </button>
+  </div>`;
+  return li;
 }
 
-const app = new TodoApp();
+function bindTaskEvents(taskElement) {
+  const removeBtn = taskElement.querySelector(".removeBtn");
+  const checkBtn = taskElement.querySelector(".checkBtn");
+
+  removeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    taskElement.remove();
+    const taskId = taskElement.id;
+    manager.removeTask(taskId);
+  });
+
+  checkBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    manager.toggleTaskState(taskElement.id, taskElement);
+  });
+}
+
+function renderTasks() {
+  taskList.innerHTML = "";
+  manager.getTasks().forEach((task) => {
+    const taskElement = createTaskElement(task.getId(), task.getText());
+    taskList.appendChild(taskElement);
+    bindTaskEvents(taskElement);
+  });
+}
 
 input.addEventListener("input", (e) => {
   inputValue = e.target.value;
@@ -135,6 +126,33 @@ form.addEventListener("submit", (e) => {
   }
   const taskText = inputValue;
   const task = new Todo(taskText);
-  app.addTask(task);
+  manager.addNewTask(task);
   input.value = "";
 });
+
+function counterCreator() {
+  let count = 0;
+
+  const getCount = () => {
+    return count;
+  };
+
+  return {
+    getCount,
+  };
+}
+
+const counter1 = counterCreator();
+const counter2 = counterCreator();
+
+const toDo1 = new Todo("taskText");
+const toDo2 = new Todo("taskText");
+
+const array = [];
+console.log(array);
+
+console.log(toDo1);
+console.log(toDo2);
+
+console.log(counter1);
+console.log(counter2);
